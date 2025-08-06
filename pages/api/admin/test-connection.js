cf73f44e4e91628ada46c0fd92d5063c63bedd66
aaprosperi <aaprosperi@gmail.com>
2025-08-05 22:27:38 -0600

    Fix Perplexity model name again
    - Changed to 'llama-3.1-sonar-small-128k-chat'
    - Previous model names were deprecated
    - This should work with current Perplexity API

--- pages/api/admin/test-connection.js
+++ pages/api/admin/test-connection.js
@@ -95,7 +95,7 @@ async function testPerplexity(apiKey) {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'llama-3.1-sonar-large-128k-online',
+        model: 'llama-3.1-sonar-small-128k-chat',
         messages: [{ role: 'user', content: 'test' }],
         max_tokens: 10,
       }),
