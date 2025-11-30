e22c3da576ef5eebc8f0fe5f115fc1185123e48f
Alfredo Arenas <aaaprosperi@gmail.com>
2025-11-29 23:29:48 -0600

    Add test page with Hello World message

---
+++ pages/test.js
@@ -0,0 +1,14 @@
+export default function Test() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      fontSize: '48px',
+      fontFamily: 'Arial, sans-serif'
+    }}>
+      Hello World
+    </div>
+  );
+}
